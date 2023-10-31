da8e8f0d7ed3b2c057110373d6056b7535ba9120
Ghostexvan <krvinhan2002@gmail.com>
2023-10-31 11:49:42 +0700

    Dinh nghia cac route cho tai nguyen hang hoa

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -3,8 +3,11 @@ const cors = require("cors");
 
 const app = express();
 
+const goodsRouter = require("./app/routes/good.route");
+
 app.use(cors());
 app.use(express.json());
+app.use("/api/goods", goodsRouter);
 
 app.get("/", (req, res) => {
     res.json({
