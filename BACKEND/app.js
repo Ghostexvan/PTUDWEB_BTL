2f9a409b3b5f9503c23b216cef00c2d325b92c4f
Ghostexvan <krvinhan2002@gmail.com>
2023-11-10 13:16:44 +0700

    Cai dat xu ly loi

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 const customesRouter = require("./app/routes/customer.route");
@@ -9,6 +10,18 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/customers", customesRouter);
 app.use("/api/staffs", staffsRouter);
+
+// Xu ly loi 404
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+})
+
+// Xu ly loi tap chung bang middleware
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 app.get("/", (req, res) => {
     res.json({
